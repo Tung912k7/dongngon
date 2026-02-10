ba8c5501127456b73dd6e058216480bdb31dffa6
Thanh Tung <81634325+Tung912k7@users.noreply.github.com>
2026-02-10 21:53:31 +0700

    Quick fix for pattern display

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -10,7 +10,7 @@ const AboutSection = () => {
           
           {/* 1. Pattern Column (Fixed Width) */}
           <div className="relative h-full w-20 md:w-24 lg:w-28 pointer-events-auto z-40 flex items-center justify-center">
-             <div className="absolute top-1/2 left-1/2 w-[100vh] h-full -translate-x-1/2 -translate-y-1/2 -rotate-90 flex items-center justify-center">
+             <div className="absolute top-1/2 left-1/2 w-[120vh] h-full -translate-x-1/2 -translate-y-1/2 -rotate-90 flex items-center justify-center">
                 <div className="relative w-full h-20 md:h-24 lg:h-28">
                   <Image 
                     src="/pattern/pattern1.png"
